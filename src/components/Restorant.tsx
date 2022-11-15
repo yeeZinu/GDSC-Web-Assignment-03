4a1c484f527defa9f8b99d0dabfc7f64f3d162ba
zinu <doglife222@gmail.com>
2022-11-15 21:06:05 +0900

    feat:맛집 페이지 제작

---
+++ src/components/Restorant.tsx
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+interface Props {
+    title: string;
+    address: string;
+    review: string;
+    imgSrc: string;
+    mapSrc: string;
+
+}
+
+
+const Restrorant = ({title, review, address, imgSrc, mapSrc}: Props) => {
+    return(
+        <Styledsection>
+            <StyledA href={mapSrc} target="_blank">
+                <StyledImg src={imgSrc} alt={title} />
+            </StyledA>
+            <StyledDiv>
+                <StyledH2>{title}</StyledH2>
+                <StyledAddress>{address}</StyledAddress>
+                <StyledP>{review}</StyledP>
+            </StyledDiv>
+            
+        </Styledsection>
+    );
+}
+
+export default Restrorant;
+
+const Styledsection = styled.section`
+    background-color: #FFD57E;
+
+    width: 300px;
+    height: 450px;
+
+    padding: 10px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+const StyledA= styled.a`
+    transition: all 0.3ms;
+    &:hover{
+        transform: scale(1.05);
+        overflow: hidden;
+        transition: all 0.3ms;
+    }
+`;
+
+const StyledImg = styled.img`
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+`;
+
+const StyledDiv = styled.div`
+padding: 10px;
+`;
+
+const StyledH2 = styled.h2`
+    color: #AC4B1C;
+`;
+
+const StyledAddress = styled.span`
+    color: #FCA652;
+`;
+
+const StyledP = styled.p`
+    color: #AC4B1C;
+`;
