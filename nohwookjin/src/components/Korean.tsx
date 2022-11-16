0ed9171e34bec2d2566042fcd6afaeaed58c2a4a
NohWookJin <jinn98@kakao.com>
2022-11-16 12:36:28 +0900

    feat: 한식 컴포넌트 추가

---
+++ nohwookjin/src/components/Korean.tsx
@@ -0,0 +1,22 @@
+import Store from "./Store";
+
+const Korean = () => {
+  return (
+    <>
+      <Store
+        name="🍽️ 국수나무"
+        location="⛳️ 서울특별시 구로구 온수동 성공회대학교 새천년관"
+        introduction="다 맛있음!"
+        imgSrc="noodleTree.png"
+      />
+      <Store
+        name="🍽️ 집밥"
+        location="⛳️ 우리집"
+        introduction="자주 애용하는 곳"
+        imgSrc="home.webp"
+      />
+    </>
+  );
+};
+
+export default Korean;
