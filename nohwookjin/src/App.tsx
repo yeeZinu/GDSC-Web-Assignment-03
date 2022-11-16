51fe2506e6a21d1b71598dec485da15291dff66e
NohWookJin <jinn98@kakao.com>
2022-11-16 12:34:11 +0900

    feat: 헤더, 메인 컴포넌트 추가

---
+++ nohwookjin/src/App.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+// components
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <StyledDiv>
+      <Header />
+      <Main />
+    </StyledDiv>
+  );
+}
+
+export default App;
+
+const StyledDiv = styled.div`
+  width: 100vw;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
