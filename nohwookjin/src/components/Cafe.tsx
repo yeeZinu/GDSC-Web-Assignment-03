8552e33f107f82f98975c4aa422395e0de1c1622
NohWookJin <jinn98@kakao.com>
2022-11-16 12:34:43 +0900

    feat: 카페탭 컴포넌트 추가

---
+++ nohwookjin/src/components/Cafe.tsx
@@ -0,0 +1,16 @@
+import Store from "./Store";
+
+const Cafe = () => {
+  return (
+    <>
+      <Store
+        name="🍽️ 블루보틀"
+        location="⛳️ 어디든"
+        introduction="놀라플로트 맛있음!"
+        imgSrc="cafe.jpeg"
+      />
+    </>
+  );
+};
+
+export default Cafe;
